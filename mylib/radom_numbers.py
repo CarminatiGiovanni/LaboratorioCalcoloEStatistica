c1b04fc0403a9264603eaaaecb94058a254cb14d
HeyJOe <jocarmi03@gmail.com>
2023-12-27 15:49:33 +0100

    exercise 4

--- mylib/radom_numbers.py
+++ mylib/radom_numbers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class RandomLCG:
   def __init__(self,A,C,M,seed=0):
     self.A = A
@@ -12,3 +14,16 @@ class RandomLCG:
   def setSeed(self,seed):
     self.seed = seed
     self.x = seed
+
+def rand_uniform(min,max,size=1):
+    if size==1: return np.random.rand()*(max-min) + min
+    return np.random.rand(size)*(max-min) + min
+
+def rand_TAC(pdf,xmin,xmax,size=1):
+    l = np.empty(size,dtype=np.float64)
+    for i in range(size):
+        randX = rand_uniform(xmin,xmax,size=1)
+        while pdf(randX) < rand_uniform(0,1,size=1):
+            randX = rand_uniform(xmin,xmax,size=1)
+        l[i] = randX
+    return l[0] if size == 1 else l
