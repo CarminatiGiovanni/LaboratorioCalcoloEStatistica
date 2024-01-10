fee46ad863ecb649a4e2159aa1aa995e4d4f050f
HeyJOe <jocarmi03@gmail.com>
2024-01-10 16:10:06 +0100

    done to 10.3

---
+++ mylib/parameters_estimator.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.stats as sc
+
+def loglikelyhood(f,X,tau):
+  return np.sum(np.log(f(X,tau)))
+
+def loglikelyhood(f,X,tau):
+  return np.sum(np.log(f(X,tau)))
+
+def exponential_pdf(X,tau):
+  return sc.expon.pdf(X,scale=tau)
