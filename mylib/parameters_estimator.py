a679197fabe4e47c02ce4df17a1e40be2ada5aa0
HeyJOe <jocarmi03@gmail.com>
2024-01-16 12:07:32 +0100

    fixed normal binned

--- mylib/parameters_estimator.py
+++ mylib/parameters_estimator.py
@@ -9,3 +9,22 @@ def likelyhood(f,X,tau):
 
 def exponential_pdf(X,tau):
   return sc.expon.pdf(X,scale=tau)
+
+## EXTENDED BINNED NNL
+# from iminuit.cost import ExtendedBinnedNLL
+# def mod_total (bin_edges, N_signal, mu, sigma, N_background, tau):
+#     return N_signal * norm.cdf (bin_edges, mu, sigma) + \
+#             N_background * expon.cdf (bin_edges, 0, tau )
+
+# bin_edges = np.linspace(min(data),max(data),sturges(N))
+
+# # data[(data>i)*(data<i + bin_width)]
+# # bin_content = np.array([len(list(filter(lambda x: x >= i and x < i + bin_width,data))) for i in bin_edges[:-1]]) # aggiustare
+# bin_content = np.array([len(data[(data>i)*(data<=g)]) for i,g in zip(bin_edges[:-1],bin_edges[1:])])
+
+# my_cost_func = ExtendedBinnedNLL (bin_content, bin_edges, mod_total)
+# my_minuit = Minuit (my_cost_func,
+#                     N_signal = N/2, mu = np.mean(data), sigma = np.std(data), # signal input parameters
+#                     N_background = N/2, tau = 5)
+
+# my_minuit.migrad()
--- mylib/radom_numbers.py
+++ mylib/radom_numbers.py
@@ -20,11 +20,21 @@ def rand_uniform(min,max,size=1):
     if size==1: return np.float64(np.random.rand()*(max-min) + min)
     return np.random.rand(size)*(max-min) + min
 
-def rand_TAC(pdf,xmin,xmax,size=1):
+# def rand_TAC(pdf,xmin,xmax,size=1):
+#     l = np.empty(size,dtype=np.float64)
+#     for i in range(size):
+#         randX = rand_uniform(xmin,xmax,size=1)
+#         while pdf(randX) < rand_uniform(0,1,size=1):
+#             randX = rand_uniform(xmin,xmax,size=1)
+#         l[i] = randX
+#     return l[0] if size == 1 else l
+
+############ ANOTHER
+def rand_TAC(pdf,xmin,xmax,ymax,size=1):
     l = np.empty(size,dtype=np.float64)
     for i in range(size):
         randX = rand_uniform(xmin,xmax,size=1)
-        while pdf(randX) < rand_uniform(0,1,size=1):
+        while pdf(randX) < rand_uniform(0,ymax,size=1):
             randX = rand_uniform(xmin,xmax,size=1)
         l[i] = randX
     return l[0] if size == 1 else l
